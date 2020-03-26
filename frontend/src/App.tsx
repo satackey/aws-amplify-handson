0add4a4df8a4a4386aee1e8247725f30cf97dbb8
satackey <21271711+satackey@users.noreply.github.com>
2020-03-26 22:19:26 +0900

    antdをインストール

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,18 @@
 import React from 'react'
 import Amplify from '@aws-amplify/core'
-// import '@aws-amplify/xr'
 
 import { withAuthenticator } from 'aws-amplify-react'
 
-import logo from './logo.svg'
 import './App.css'
 import awsconfig from './aws-exports'
+
+import Button from 'antd/es/button'
 
 Amplify.configure(awsconfig)
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    <Button type="primary">Button</Button>
   </div>
 )
 
